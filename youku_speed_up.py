1ac250a9c8ea3a0ba7a4e31b8f2baa1104f46795
gaosen <0x5e@sina.cn>
2017-07-02 02:45:25 +0800

    去除无用参数

--- youku_speed_up.py
+++ youku_speed_up.py
@@ -6,32 +6,30 @@ password = '000000'
 
 import requests
 import json
-import time
 import re
 import hashlib
-import random
 import logging
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
 session = requests.session()
-session.headers = {
-	'Referer': 'http://vip.youku.com/vips/priSpeedup.html',
-	'User-Agent': 'Mozilla/5.0'
+
+actions = {
+	# 'getConfig': 'http://account.youku.com/getConfig.json',
+	'refreshFormToken': 'http://account.youku.com/refreshFormToken.json',
+	'login': 'http://account.youku.com/login/confirm.json',
+
+	# http://static.youku.com/paymentcenter/vip-pc/build/js/libs/config-build.js
+	'speed_up': 'http://vip.youku.com/?c=ajax&a=ajax_do_speed_up', # 宽带加速
+	'switch_service_url': 'http://vip.youku.com/?c=ajax&a=ajax_speedup_service_switch', # 加速服务开关
+	'speed_status': 'http://vip.youku.com/ajax/speedup/get_status.jsonp', # 用户的宽带加速状态判断
 }
 
 def loads_jsonp(_jsonp):
 	return json.loads(re.match('.*?({.*}).*', _jsonp, re.S).group(1))
 
 def login():
-	params = {
-		'pid': '20160607PLF000287',
-		'bizType': 'login',
-		'jsonpCallback': '__jp0',
-	}
-
-	# response = session.get(url = 'http://account.youku.com/getConfig.json')
-	response = session.get(url = 'http://account.youku.com/refreshFormToken.json', params = params)
+	response = session.get(actions['refreshFormToken'])
 	token = loads_jsonp(response.text)['data']['formtoken']
 	logging.debug(token)
 
@@ -43,19 +41,12 @@ def login():
 		'formtoken': token,
 		'passport': passport,
 		'password': md5_password,
-		'buid': 'youku',
-		'pid': '20160607PLF000287',
-		'mode': 'popup',
-		'sendCodeType': 'mobileCode',
-		'state': 'false',
-		'rememberMe': 'true',
 		'loginType': 'passport_pwd',
-		'UA': 'FFFF00000000016514CE:%s:%s'%(time.time() * 1000, random.random()),
+		'UA': 'FFFF00000000016514CE',
 		'jsToken': '0',
-		'jsonpCallback': '__jp1',
 	}
 	
-	response = session.get(url = 'http://account.youku.com/login/confirm.json', params = params)
+	response = session.get(actions['login'], params = params)
 	data = loads_jsonp(response.text)
 	if data['result'] == 'success':
 		logging.info('登录成功')
@@ -65,19 +56,19 @@ def login():
 		return False
 
 def get_status():
-	response = session.get(url = 'http://vip.youku.com/ajax/speedup/get_status.jsonp')
+	response = session.get(actions['speed_status'])
 	data = loads_jsonp(response.text)['result']
 	return data
 
 def speed_up():
-	response = session.get(url = 'http://vip.youku.com/?c=ajax&a=ajax_do_speed_up')
+	response = session.get(actions['speed_up'])
 	data = response.json()
 	logging.info(data['msg'])
 	if 'operator_error_msg' in data['result']:
 		logging.info(data['result']['operator_error_msg'])
 
 def switch():
-	response = session.get(url = 'http://vip.youku.com/?c=ajax&a=ajax_speedup_service_switch')
+	response = session.get(actions['switch_service_url'])
 	data = response.json()
 	logging.info(data['msg'])
 
@@ -87,14 +78,14 @@ def main():
 
 	status = get_status()
 	if status['speed_up_state'] == True:
-		logging('加速服务已开启')
+		logging('宽带加速已开启')
 		return
 
 	if status['speed_up_switch'] == 2:
-		logging.info('开启[加速服务]...')
+		logging.info('开启[加速服务开关]...')
 		switch()
 
-	logging.info('开启[宽带加速服务]...')
+	logging.info('开启[宽带加速]...')
 	speed_up()
 
 if __name__ == '__main__':
